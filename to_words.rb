799b96d79e0a09fc38e0833c7dcfcc79050be38f
Martin.Popelak <martin.popelak@railsworx.org>
2009-07-18 10:07:52 +0200

    Implementace dvojky

--- to_words.rb
+++ to_words.rb
@@ -1,9 +1,9 @@
 class Fixnum
     def to_words
       vysledek = Array.new
-      cisla = %w(~ jedna)
+      cisla = %w(~ jedna dva)
       case self
-      when 1
+      when 1..2
         vysledek << cisla[self]
       end
       vysledek.join(" ")

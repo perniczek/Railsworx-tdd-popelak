ca2764a67b0c6e9ae1cde527233f1657d0754032
Martin.Popelak <martin.popelak@railsworx.org>
2009-07-18 10:06:20 +0200

    Test s dvojkou

--- test/to_words_test.rb
+++ test/to_words_test.rb
@@ -5,4 +5,8 @@ class ToWordsTest < Test::Unit::TestCase
   def test_vrati_jednucku
     assert_equal "jedna", 1.to_words
   end
+  
+  def test_vrati_dvojku
+    assert_equal "dva", 2.to_words
+  end
 end

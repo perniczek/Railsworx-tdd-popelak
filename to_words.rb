12590fda1f56f666f4e713ee3075a983bc99b0a6
Martin.Popelak <martin.popelak@railsworx.org>
2009-07-18 10:05:40 +0200

    Implementace jednicky

--- to_words.rb
+++ to_words.rb
@@ -1,4 +1,11 @@
 class Fixnum
-  def to_words
-  end
+    def to_words
+      vysledek = Array.new
+      cisla = %w(~ jedna)
+      case self
+      when 1
+        vysledek << cisla[self]
+      end
+      vysledek.join(" ")
+    end
 end

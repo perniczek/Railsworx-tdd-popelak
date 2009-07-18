da49a04606491d60904a3cdbd813165e20365de9
Martin.Popelak <martin.popelak@railsworx.org>
2009-07-18 10:41:25 +0200

    implementace 11

--- to_words.rb
+++ to_words.rb
@@ -2,9 +2,12 @@ class Fixnum
     def to_words
       vysledek = Array.new
       cisla = %w(nula jedna dva tri ctyri pet sest sedm osm devet deset)
+      nepravidelne = %w(~ jedenact)
       case self
       when 0..10
         vysledek << cisla[self]
+      when 11..19
+        vysledek << nepravidelne[self % 10]
       end
       vysledek.join(" ")
     end

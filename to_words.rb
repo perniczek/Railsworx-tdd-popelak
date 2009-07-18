7ea481503db51e2e989e63230ccf781274f7761b
Martin.Popelak <martin.popelak@railsworx.org>
2009-07-18 10:17:39 +0200

    implementace 5

--- to_words.rb
+++ to_words.rb
@@ -1,9 +1,9 @@
 class Fixnum
     def to_words
       vysledek = Array.new
-      cisla = %w(~ jedna dva tri ctyri)
+      cisla = %w(~ jedna dva tri ctyri pet)
       case self
-      when 1..4
+      when 1..5
         vysledek << cisla[self]
       end
       vysledek.join(" ")

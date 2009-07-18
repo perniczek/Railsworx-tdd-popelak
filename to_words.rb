429fe721672650710e4512ff956824a61b3fc61a
Martin.Popelak <martin.popelak@railsworx.org>
2009-07-18 11:14:22 +0200

    Refaktorizace kodu

--- to_words.rb
+++ to_words.rb
@@ -1,18 +1,16 @@
 class Fixnum
     def to_words
-      vysledek = Array.new
       cisla = %w(nula jedna dva tri ctyri pet sest sedm osm devet deset)
       nepravidelne = %w(~ jedenact dvanact trinact ctrnact patnact sestnact sedmnact osmnact devatenact)
       desitky = %w(~ ~ dvacet tricet ctyricet padesat sedesat sedmdesat osmdesat davadesat sto)
       case self
       when 0..10
-        vysledek << cisla[self]
+        return cisla[self]
       when 11..19
-        vysledek << nepravidelne[self % 10]
+        return nepravidelne[self % 10]
       when 20..100
         zbytek = self % 10
-        vysledek << (desitky[self / 10] + (cisla[zbytek] unless zbytek == 0).to_s)
+        return (desitky[self / 10] + (cisla[zbytek] unless zbytek == 0).to_s)
       end
-      vysledek.join(" ")
     end
 end
